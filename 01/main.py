401bfa14d897ed68a759d450663d992287770f95
NikosMouzakitis <mzktsn@gmail.com>
2021-02-13 03:13:27 +0200

    reading the FASTA file into dictionary of Python
    Signed-off-by: NikosMouzakitis <mzktsn@gmail.com>

--- 01/main.py
+++ 01/main.py
@@ -16,3 +16,35 @@ for record in SeqIO.parse(handle,"fasta"):
 	print(record.seq)
 	print(record.description)
 handle.close()
+
+#########################################
+#	using the Sequences.fasta	#
+#########################################
+
+f="Sequences.fasta"
+handle=open(f,"rU")
+print("handle acquired on %s file."%(f))
+
+lines=handle.readlines()
+
+seq_dict={}
+seq_name=None
+
+for line in lines:
+	if line[0]=='>':	#special for FASTA start
+		seq_name=line[1:].strip()
+		seq_dict[seq_name]=""
+	else:
+		if seq_name:
+			seq_dict[seq_name]+=line.strip()
+
+
+print(seq_dict)
+
+for i in seq_dict.keys():
+	print("Gene:>>")
+	print(i)
+	print("Sequence:>>")
+	print(seq_dict[i])
+
+handle.close()
