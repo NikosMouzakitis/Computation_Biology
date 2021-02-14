cda6e2e0316bf14f64b38a14d35d0da8e74a07fe
NikosMouzakitis <mzktsn@gmail.com>
2021-02-14 02:28:13 +0200

    using iterator regular expresions
    Signed-off-by: NikosMouzakitis <mzktsn@gmail.com>

--- 02/main.py
+++ 02/main.py
@@ -15,6 +15,29 @@ print(re.findall(restrictionEnzymes['bamH1'][0],sequence1))
 print(re.findall(restrictionEnzymes['nci1'][0],sequence1))
 print(re.findall(restrictionEnzymes['scrF1'][0],sequence1))
 
-promoter="agtc..taat"	#dot,means it can be any of the characters(in our nucleod case any of(ATGC).
-promoter="agtc.{5,12}taat"#allows a variable lenght for the .,between five and twelve bases.
+sequence2 = 'cccccttgacaccccccccccccccccctataatccccc'
+sequence3 = 'cccccttgacaccccccccccccccccccccctataatccccc'
 
+promoter="ttgaca..tataat"	#dot,means it can be any of the characters(in our nucleod case any of(ATGC).
+promoter2="ttgaca.{5,30}taat"#allows a variable lenght for the .,between five and twelve bases.
+print("Finding '"+promoter2+"' in: "+sequence2)
+print(re.findall(promoter2, sequence2))
+
+print("Finding '"+promoter2+"' in: "+sequence3)
+print(re.findall(promoter2, sequence3))
+
+
+
+#using iterators
+
+print(re.finditer(promoter2, sequence2))
+matches = re.finditer(promoter2, sequence2)
+print("using the Iterator")
+print("Finding '"+promoter2+"' in: "+sequence2)
+
+for m in matches:
+	print(m.group())
+	print(m.start(), m.end())
+	print(sequence2[m.start()])
+	print(sequence2[m.end()-1])
+	print(sequence2[m.start():m.end()])
