e7418b57a5d334afdc7644c067afe6d552294be6
NikosMouzakitis <mzktsn@gmail.com>
2021-02-15 17:25:58 +0200

    09

--- 09_next_generation_sequencing-deep_sequencing/main.py
+++ 09_next_generation_sequencing-deep_sequencing/main.py
@@ -1,12 +1,28 @@
+import pysam
+import os.path
 import subprocess
+
+def sort_and_index(bam_filename):
+	prefix,suffix=os.path.splitext(bam_filename)
+	
+	#generated sorted bam file
+	sorted_bamfilename=prefix+".sorted.bam"
+	#sort it using pysam's output
+	print("generated resorted BAM:"+sorted_bamfilename)
+	pysam.sort("-o",sorted_bamfilename,bam_filename)
+
+	#index it
+	print("Index BAM:"+sorted_bamfilename)
+	pysam.index(sorted_bamfilename)
+
+	return sorted_bamfilename
+
+
 #automate alignment
-#with open('e_coli_10000snp.sam',"w") as output_sam:
-#	subprocess.check_call(['bwa','mem','NC_008253.fna','e_coli_10000snp.fq'], stdout=output_sam)
-print("1")
+
 output_sam=open('e_coli_10000snp.sam',"w")
 subprocess.check_call(['bwa','mem','NC_008253.fna','e_coli_10000snp.fq'], stdout=output_sam)
 output_sam.close()
-print("2")
 with open("e_coli_10000snp.bam","w") as output_bam:
 	subprocess.check_call(['samtools','view','-b','-S',"e_coli_10000snp.sam"],stdout=output_bam)
 
@@ -14,7 +30,9 @@ with open("e_coli_10000snp.bam","w") as output_bam:
 #we could continue create more wrappers like that to include the whole 
 #samtools commands as we see in the readme file.
 
-import os.path
-import pysam
-
 print("working with pysam")
+output_sorted_bamfilename=sort_and_index("e_coli_10000snp.bam")
+print(output_sorted_bamfilename)
+
+
+
